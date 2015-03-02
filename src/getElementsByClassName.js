22687f79210f29ca76ec3bce4e9723abd5774ac4
Eddie Kong <eddiek93@gmail.com>
2015-03-02 13:01:22 -0800

    Submission 1

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,23 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
   // your code here
+  var result = [];
+  var theDom = [document.body];
+
+  var traverse = function(element){
+  	_.each(element, function(e){
+  		if (_.contains(e.classList, className))
+  		{
+  			result.push(e);
+  		}
+  		traverse(e.childNodes);
+  	});
+  };
+
+  traverse(theDom);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(typeof obj === 'boolean' || typeof obj == 'number'){
+    return String(obj);
+  } else if (typeof obj === 'string'){
+    return "\"" + obj + "\"";
+  }else if (obj === null){
+    return 'null';
+	} else if (typeof obj === 'object' && !Array.isArray(obj)){
+    var dstring = '';
+    _.each(obj, function(val, key){
+      if(stringifyJSON(val) !== undefined) {
+        dstring += stringifyJSON(key) + ":" + stringifyJSON(val) + ",";
+      }
+    });
+    return '{' + dstring.slice(0, dstring.length -1) + '}';
+	} else if (typeof obj === 'object' && Array.isArray(obj)) {
+    var sstring = '';
+		_.each(obj, function(item){
+      sstring += (stringifyJSON(item) + ",");
+    });
+    return '[' + sstring.slice(0, sstring.length -1) + ']';
+	}
 };
